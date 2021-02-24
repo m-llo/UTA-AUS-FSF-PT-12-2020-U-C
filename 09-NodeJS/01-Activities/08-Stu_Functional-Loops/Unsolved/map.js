249a9fb2c9ac32c8766f6117d3414af58334c5a6
m-llo <markalloyd21@gmail.com>
2021-02-24 16:05:41 -0600

    updated session 9.1

--- 09-NodeJS/01-Activities/08-Stu_Functional-Loops/Unsolved/map.js
+++ 09-NodeJS/01-Activities/08-Stu_Functional-Loops/Unsolved/map.js
@@ -1,6 +1,7 @@
 const originalArray = [1, 3, 2, 5, 10];
 
-// doubledArray is equal to a new array of numbers multiplied by 2 and returned by map(). map() will use an anonymous function as a condition. 
+// doubledArray is equal to a new array of numbers multiplied by 2 and returned by map(). 
+// map() will use an anonymous function as a condition. 
 const doubledArray = originalArray.map(function(data) {
   // The anonymous function returns each number multiplied by 2. The returned result is added to a new array.
   return data * 2;
@@ -11,7 +12,8 @@ console.log(originalArray);
 
 
 // TODO: Describe how map is working in the example below. What will the value of tripledArray be?
-const tripledArray = originalArray.map(data => data * 3);
+const tripledArray = originalArray.map(data =>  data * 3);
+console.log(tripledArray)
 
 // TODO: Describe how map is working in the example below. What will the value of oddOrEven be?
 const oddOrEven = originalArray.map(num => {
@@ -21,3 +23,5 @@ const oddOrEven = originalArray.map(num => {
     return 'odd'
   }
 });
+
+console.log(oddOrEven)
--- 09-NodeJS/01-Activities/10-Stu_Template-Literals/Unsolved/index.js
+++ 09-NodeJS/01-Activities/10-Stu_Template-Literals/Unsolved/index.js
@@ -1,14 +1,21 @@
 // music should be an object with title, artist, and album properties
 const music = {
   // code here
+  title: "Meet The Parents ",
+  artist: "Jay-Z",
+  album: "The Blueprint 2", 
 };
 
 // Write code between the <div> tags to output the data from the music object above.
 // Use an h2 element for the title and a p element for artist and title
+let keys = Object.keys(music);
 const songSnippet = `
   <div class="song">
-
+  <h2>${keys[0]}: ${music.title}</h2>
+  <p>${keys[1]}: ${music.artist}</p>
+  <p>${keys[2]}: ${music.album}</p>
   </div>
+
 `;
 
 const element = document.getElementById("music");
