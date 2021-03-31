eca56c86f0ec85d74f1daedf9888ff6761b84f6a
m-llo <markalloyd21@gmail.com>
2021-03-31 17:10:53 -0500

    week 14.1

---
+++ 14-MVC/01-Activities/08-Stu_Data-Serialization/Unsolved/controllers/dish-routes.js
@@ -0,0 +1,35 @@
+const router = require('express').Router();
+const sequelize = require('../config/connection');
+const Dish = require('../models/Dish');
+
+router.get('/', async (req, res) => {
+  // TODO: Build out this route so that it serializes all of the dish objects that it receives. See the 'get' route below for a hint.
+  try {
+    
+    const dishData = await Dish.findAll();
+      const dishes = dishData.map((dish) => dish.get({plain: true}))
+    if(!dishData) {
+      res.status(404).json({message: 'No dish with this id!'});
+      return;
+  } res.render('all', {dishes});
+    } catch (err) {
+       res.status(500).json(err);
+  }
+});
+
+// route to get one dish
+router.get('/dish/:id', async (req, res) => {
+  try{ 
+      const dishData = await Dish.findByPk(req.params.id);
+      if(!dishData) {
+          res.status(404).json({message: 'No dish with this id!'});
+          return;
+      }
+      const dish = dishData.get({ plain: true });
+      res.render('dish', dish);
+    } catch (err) {
+        res.status(500).json(err);
+    };     
+});
+
+module.exports = router;
